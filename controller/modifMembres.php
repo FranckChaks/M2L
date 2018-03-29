a6ddaa2b633c82ff8ea078fddb475bd8447664fb
draikan <31989340+draikan@users.noreply.github.com>
2018-03-29 15:01:46 +0200

    rectification update infos membres

--- controller/modifMembres.php
+++ controller/modifMembres.php
@@ -1,6 +1,25 @@
 <?php
 require "model/modifMembresModel.php";
-    $id = $_GET["id"];
+    $id = (int)$_GET["id"];
     $r = get_infos_membres($id);
+
+if (isset($_POST['modif_infos_base'])) {
+    if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['email'])) {
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $email = $_POST['email'];
+        modif_infos_base($id,$nom, $prenom, $email);
+    }
+    header('Location:espaceGestion');
+}
+if (isset($_POST['modif_infos_credit'])) {
+    if (isset($_POST['nom']) && !empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['email'])) {
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $email = $_POST['email'];
+
+        modif_infos_base($id,$nom, $prenom, $email);
+    }
+}
 
 require "view/modifMembresView.php";
--- model/modifMembresModel.php
+++ model/modifMembresModel.php
@@ -6,3 +6,23 @@ function get_infos_membres($id)
     $requete->execute();
     return $requete->fetch();
 }
+function modif_infos_base($id, $nom, $prenom, $email)
+{
+    global $bdd;
+    $req = $bdd->prepare("UPDATE salarie SET nom = :nom, prenom = :prenom, email = :email WHERE id_s = :id");
+    $req->bindValue(":id", $id, PDO::PARAM_INT);
+    $req->bindValue(":nom", $nom, PDO::PARAM_STR);
+    $req->bindValue(":prenom", $prenom, PDO::PARAM_STR);
+    $req->bindValue(":email", $email, PDO::PARAM_STR);
+    $req->execute();
+    $message = "Modifications effectuées";
+    return $message;
+}
+function modif_infos_credit(){
+    global $bdd;
+//    $req = $bdd->prepare("UPDATE user SET ban = 1 WHERE id_u = :id_u");
+//    $req->bindValue(":nom", $nom, PDO::PARAM_STR);
+//    $req->bindValue(":prenom", $prenom, PDO::PARAM_STR);
+//    $req->bindValue(":email", $email, PDO::PARAM_STR);
+//    $req->execute();
+}
