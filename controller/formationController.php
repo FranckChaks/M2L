89e6506daf9da9e97569af98d8b61aef78c4b499
FranckChaks <franck1891@hotmail.fr>
2018-04-12 12:47:30 +0200

    update credit

--- controller/formationController.php
+++ controller/formationController.php
@@ -10,8 +10,25 @@
 
     if(isset($_GET['ajouter'])){
         $id_f = $_GET['ajouter'];
-        addFormation($_SESSION['id'], $id_f);
-        header("location:EspacePerso");
+        $formcredit = formationCredit($id_f)[0]; //credit formation
+        $credit = creditLeft()[0];              //credit salarie
+        $credit = $credit - $formcredit;
+
+        var_dump($credit);
+        die();
+        updateCredit($credit);
+
+        if($credit < 0){                //comparaison si le salarié a assez de crédits pour s'inscrire à la formation
+            echo "Vous n'avez plus assez de crédits.";
+        }
+        else{               //là on ajoute la formation au salarié
+            updateCredit($credit);
+
+            addFormation($_SESSION['id'], $id_f);
+            header("location:EspacePerso");
+        }
+
+
     }
 
     $form = displayFormation();
