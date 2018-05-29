a54c3dd612987e086d3f33eb27731f246a932322
draikan <31989340+draikan@users.noreply.github.com>
2018-05-29 22:03:44 +0200

    debug affiliation employés

--- controller/modifMembres.php
+++ controller/modifMembres.php
@@ -3,7 +3,9 @@ require "model/modifMembresModel.php";
     $id = (int)$_GET["id"];
     $r = get_infos_membres($id);
     $s = get_infos_chefs($id);
-    $infoPerso = displayInfo();
+    $tableInfoPerso = displayInfo()[0]; //suppression d'un niveau de table ( array in array )
+    $infoPerso = $tableInfoPerso[9]; //selection de l'information de la 10ème case du tableau ( commence à 0 ) qui correspond à id_c  
+
 //    $t = get_tous_membres();
 //    $i = count($t);
     //while($i > 0) {
@@ -20,27 +22,23 @@ if (isset($_POST['modif_infos_base'])) {
         $prenom = $_POST['prenom'];
         $email = $_POST['email'];
         
-        if(isset($_SESSION['id']) AND $_SESSION['id'] == $id OR isset($_SESSION['lvl']) AND $_SESSION['lvl'] < 2){ // Si id session est l'id de la modification ( modif perso ) ou si le level de la session est inférieur à 2 
-            $id_c = $infoPerso['id_c']; // Alors id_c prends sa valeure originale 
+        if($_SESSION['id'] == $id || $_SESSION['lvl'] < 2){ // Si id session est l'id de la modification ( modif perso )
+            $id_c = $infoPerso; // Alors id_c prends sa valeure originale 
+        }else{
+            header('Location:EspacePerso');
         }
         
-        if(isset($_SESSION['lvl']) AND $_SESSION['lvl'] == 2) { //si admin et si modification non sur sa fiche perso alors id_c prends la valeure du select
-            if($_SESSION['id'] !==$id ){
-                $id_c = $_POST['chef'];
-            }else{
-                $id_c=0;
-            }
+        if($_SESSION['lvl'] == 2 AND $_SESSION['id'] == $id ) { //si admin et si modification non sur sa fiche perso alors id_c prends la valeure du select
+            $id_c='0'; 
         }
-        if(isset($_SESSION['lvl']) AND $_SESSION['lvl'] < 2) {  
-            if($_SESSION['id'] !==$id ){
-                $id_c = $_POST['chef'];
-            }else{
-                $id_c=0;
-            }
+        if($_SESSION['lvl'] == 2 AND $_SESSION['id'] !== $id){
+            (int)$id_c = $_POST['chef'];
         }
+        
         modif_infos_base($id,$nom, $prenom, $email, $id_c);
     }
     header('Location:espaceGestion');
+        
 }
 if (isset($_POST['modif_infos_credit'])) {
 }
